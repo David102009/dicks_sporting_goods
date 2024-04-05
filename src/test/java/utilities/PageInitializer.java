884022f744bd4875e25c1bae46d2ad9dd1ab0202
Sunny Su <sunnysu@sunnys-air.mynetworksettings.com>
2024-04-04 21:46:08 -0400

    secondary changes

--- src/test/java/utilities/PageInitializer.java
+++ src/test/java/utilities/PageInitializer.java
@@ -20,7 +20,7 @@ public class PageInitializer extends BaseClass{
 	public static PickupAndDeliveryPage pAD;
 	public static ReturnsAndExchangesPage rE;
 	public static ServicesPage sP;
-  public static CommonPage cP ;
+ 
 	public static createAccountPage cAP ;
 	public static forgotYourPasswordPage fYP;
 	public static cartPage cOP;
